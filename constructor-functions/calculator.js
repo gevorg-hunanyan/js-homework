b50c4f9fcd0a5f9a1327d136ac16190053101369
Gevorg Hunanyan <gevorg.hunanyan.999@gmail.com>
2023-09-09 02:29:32 +0400

    created calculator.js

---
+++ constructor-functions/calculator.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function Calculator() {
+}
+
+Calculator.prototype.add = (a, b) => a + b;
+Calculator.prototype.subtract = (a, b) => a - b;
+Calculator.prototype.multiply = (a, b) => a * b;
+Calculator.prototype.divide = (a, b) => a / b;
